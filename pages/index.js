fbb22eaba0789c231e5e396b4f12b5069669eeb2
FredVaagen <Fredrickvaagen@hotmail.com>
2021-04-19 16:03:42 +0200

    sadasdas

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home({ establishments }) {
                 ...params.InputProps,
                 startAdornment: (
                   <InputAdornment position="start">
-                    <SearchIcon />
+                   
                   </InputAdornment>
                 ),
               }}
