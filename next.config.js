92f96579eb6d31343df8417fb3662f8deb2ad7d7
FredVaagen <Fredrickvaagen@hotmail.com>
2021-04-20 20:04:33 +0200

    jiimoom

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     images: {
-      domains: ['holidaze-project-exam-2-fredrick-vaagen.s3.eu-north-1.amazonaws.com']
+      domains: ['holidaze-project-exam-2.s3.eu-north-1.amazonaws.com']
     },
   }
