8688d2e0c3ce89debe37717d3fd724b9124d74e5
FredVaagen <Fredrickvaagen@hotmail.com>
2021-05-07 02:25:23 +0200

    asdasdas

--- components/admin/establishment/EditEstablishment.js
+++ components/admin/establishment/EditEstablishment.js
@@ -13,11 +13,11 @@ const EditEstablishment = (props) => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
-  let [wifiChecked, setWifiChecked] = useState(true);
+
   if (props.facilities.wifi == null || props.facilities.wifi == false) {  
-    [wifiChecked, setWifiChecked] = useState(false);
+   let [wifiChecked, setWifiChecked] = useState(false);
   
-  }
+  } else let [wifiChecked, setWifiChecked] = useState(true);
 
   let [accesibleChecked, setAccesibleChecked] = useState(true);
   if (
