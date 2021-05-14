0073813823e5ecda90652d0747d31fba294573de
FredVaagen <Fredrickvaagen@hotmail.com>
2021-05-14 15:55:41 +0200

    sadsa

--- pages/establishments/index.js
+++ pages/establishments/index.js
@@ -33,7 +33,7 @@ export default function EstablishmentsPage({ establishments }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const resAscName = await fetch(`${BASE_URL}/establishments?_sort=name:asc`);
   const resDescName = await fetch(`${BASE_URL}/establishments?_sort=name:desc`);
   const resAscPrice = await fetch(`${BASE_URL}/establishments?_sort=price:asc`);
@@ -48,6 +48,6 @@ export async function getStaticProps() {
 
   return {
     props: { establishments, descName, ascPrice, descPrice },
-    revalidate: 1,
+    
   };
 }
